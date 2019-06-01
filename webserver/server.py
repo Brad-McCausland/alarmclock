f7cefbdde80400978c9f9f40e8f6f2db2e630ad1
= <=>
2019-05-31 17:29:11 -0700

    Fix bug in demo server. Now works just fine.

--- webserver/server.py
+++ webserver/server.py
@@ -4,6 +4,7 @@ import socketserver
 PORT = 8080
 Handler = http.server.SimpleHTTPRequestHandler
 
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print("serving at port", PORT)
-    httpd.serve_forever()
+httpd = socketserver.TCPServer(("", PORT), Handler)
+#TODO: INTERRUPT ALARM SEQUENCE HERE
+print("serving at port", PORT)
+httpd.serve_forever()
